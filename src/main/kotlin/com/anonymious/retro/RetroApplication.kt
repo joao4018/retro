1ef6cf70b82783460a758359264448f3ac1086ca
joao4018 <joao4018@gmail.com>
2024-03-09 16:24:46 -0300

    create dev enviroment

--- src/main/kotlin/com/anonymious/retro/RetroApplication.kt
+++ src/main/kotlin/com/anonymious/retro/RetroApplication.kt
@@ -1,20 +1,12 @@
 package com.anonymious.retro
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean
 
 
-@EnableAutoConfiguration
+
+@SpringBootApplication
 class RetroApplication
-
-@Bean(name = ["entityManagerFactory"])
-fun sessionFactory(): LocalSessionFactoryBean {
-	val sessionFactory = LocalSessionFactoryBean()
-
-	return sessionFactory
-}
 
 fun main(args: Array<String>) {
 	runApplication<RetroApplication>(*args)
